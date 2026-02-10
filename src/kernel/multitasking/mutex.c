da09b7eeebbef9d3cba7e6226ff24f7d6d7b0918
EtienneMaire37 <etiennemaire629@gmail.com>
2026-02-10 08:13:14 +0100

    Fixed a bug with mutexes

--- src/kernel/multitasking/mutex.c
+++ src/kernel/multitasking/mutex.c
@@ -14,7 +14,7 @@ void acquire_mutex(mutex_t* mutex)
  			return;
 
  		if (multitasking_enabled)
- 			futex_wait((uint64_t)virtual_to_physical((uint64_t*)get_cr3(), (virtual_address_t)mutex), *mutex);
+ 			futex_wait((uint64_t)virtual_to_physical((uint64_t*)(get_cr3() + PHYS_MAP_BASE), (virtual_address_t)mutex), *mutex);
  		else
  			__builtin_ia32_pause();
     }
