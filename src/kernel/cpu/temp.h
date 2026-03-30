17ac7eef92b49efadbc213c1a2f004ac320df570
EtienneMaire37 <etiennemaire629@gmail.com>
2026-03-30 15:59:13 +0200

    Minor changes to the temperature reading code

--- src/kernel/cpu/temp.h
+++ src/kernel/cpu/temp.h
@@ -12,9 +12,9 @@ static inline void cpu_init_sensor()
 {
     // * The processor supports a digital thermal sensor if CPUID.06H.EAX[0] = 1.
     // * -- Intel manual vol 3B 15.8.5.1
-    if (cpuid_highest_extended_function_parameter >= 0x6)
+    if (cpuid_highest_function_parameter >= 0x6)
     {
-        uint8_t eax = 0, ebx, ecx, edx;
+        uint32_t eax = 0, ebx, ecx, edx;
         cpuid(0x6, eax, ebx, ecx, edx);
         temperature_sensor = eax & 1;
     }
@@ -36,6 +36,8 @@ static inline uint8_t cpu_read_temp()
     if (((val >> 31) & 1) == 0)
         return 0;
     uint8_t readout = (val >> 16) & 0x7f;
+    if (readout > tcc_activation_temperature)
+        return 0;
     // uint8_t resolution = (val >> 27) & 0x0f;
     return tcc_activation_temperature - readout;
 }
