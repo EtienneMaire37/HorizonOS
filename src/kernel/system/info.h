7f2048ff17e8a5a6d64d6095e61bd4fa2693d58e
EtienneMaire37 <etiennemaire629@gmail.com>
2026-04-13 15:30:33 +0200

    chore: update kernel version

--- src/kernel/system/info.h
+++ src/kernel/system/info.h
@@ -2,7 +2,7 @@
 
 static const char* sys_sysname = "HorizonOS";
 static const char* sys_nodename = "hos-pc";
-static const char* sys_release = "hos-0.1.0";
-static const char* sys_version = "HorizonOS 0.1.0 (2026 Mar 22)";
+static const char* sys_release = "0.1.1";
+static const char* sys_version = "HorizonOS 0.1.1 (2026-04-13)";
 static const char* sys_machine = "x86_64";
 static const char* sys_domainname = "";
